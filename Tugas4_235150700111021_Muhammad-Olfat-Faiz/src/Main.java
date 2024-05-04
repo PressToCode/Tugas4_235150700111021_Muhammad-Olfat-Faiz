aed05b4739a4be3d3fb7f1102b109c574e7de62b
PressToCode <137992170+PressToCode@users.noreply.github.com>
2024-05-04 12:22:11 +0700

    Update Main.java
    Updated for user-input feature :v

--- Tugas4_235150700111021_Muhammad-Olfat-Faiz/src/Main.java
+++ Tugas4_235150700111021_Muhammad-Olfat-Faiz/src/Main.java
@@ -1,5 +1,66 @@
+// import java.util.LinkedList;
+// import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // ! Kode dibawah digunakan jika ingin mengimplementasi user-input
+        // Import scanner dan LinkedList terlebih dahulu sebelum menjalankan kode
+        // Scanner input = new Scanner(System.in);
+
+        // System.out.println("---- Sistem Input Buku ----");
+        // LinkedList<Book> bukuUser = new LinkedList<>();
+
+        // loop: while(true) {
+        //     System.out.print("Nama buku\t\t: ");
+        //     String namaBuku = input.nextLine();
+
+        //     System.out.print("Author\t\t\t: ");
+        //     String author = input.nextLine();
+
+        //     System.out.print("Tahun rilis\t\t: ");
+        //     int year = input.nextInt();
+        //     input.nextLine();
+
+        //     String spesifik = "";
+        //     run: while(true) {
+        //         System.out.print("Jenis buku (TB/N/M)\t: ");
+        //         switch (input.nextLine().toLowerCase()) {
+        //             case "tb":
+        //                 System.out.print("Subjek\t\t\t: ");
+        //                 spesifik = input.nextLine();
+        //                 bukuUser.offer(new TextBook(namaBuku, author, year, spesifik));
+        //                 break run;
+    
+        //             case "n":
+        //                 System.out.print("Genre\t\t\t: ");
+        //                 spesifik = input.nextLine();
+        //                 bukuUser.offer(new TextBook(namaBuku, author, year, spesifik));
+        //                 break run;
+                    
+        //             case "m":
+        //                 System.out.print("Category\t\t: ");
+        //                 spesifik = input.nextLine();
+        //                 bukuUser.offer(new TextBook(namaBuku, author, year, spesifik));
+        //                 break run;
+                
+        //             default:
+        //                 System.out.println("invalid");
+        //                 break;
+        //         }
+        //     }
+
+        //     System.out.print("Pinjam buku? (Y/N)\t: ");
+        //     if(input.nextLine().equalsIgnoreCase("Y")) {
+        //         bukuUser.getLast().setBorrowed(true);
+        //     }
+
+        //     System.out.print("Berhenti input? (X untuk berhenti): ");
+        //     if(input.nextLine().equalsIgnoreCase("x")) {
+        //         input.close();
+        //         break loop;
+        //     }
+        // }
+
         // Objek Textbook
         TextBook buku_A = new TextBook("Java Programming", "John Smith", 2020, "Computer Science");
 
@@ -21,6 +82,12 @@ public class Main {
             currentBook.displayInfo();
             System.out.println();
         }
+
+        // Output user books (Jika menggunakan user input)
+        // for(Book currentBook : bukuUser) {
+        //     currentBook.displayInfo();
+        //     System.out.println();
+        // }
 
         System.out.println("---- Borrowing & Returning Books ----");
         // Kasus A: Objek berjenis Textbook telah dipinjam dan berhasil dikembalikan
